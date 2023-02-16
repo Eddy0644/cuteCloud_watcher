12ff714c2d1cbe049270c98e4cda150b267f7f80
netcy <a@nc32.top>
2023-02-16 13:33:46 +0800

    progressing...

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 const fetch = require("node-fetch");
-
-//TODO:暂时以
+let traffic_db={};
+let traffic_db_stat={
+    initialTimestamp:0,
+    records:0
+};
+//TODO:暂时以data子项的键名为索引
 async function pullData(){
-    let fetch=require("node-fetch");
-    let response = fetch("https://www.cutecloud.net/user/ajax_data/chart/index_node_traffic", {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    fetch("https://www.cutecloud.net/user/ajax_data/chart/index_node_traffic", {
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Cookie':require("secret").cookie
         },
     }).then(response=>response.json()).then(response=>{
         if(response.ret !== 1)return false;
+        const timestamp=Date.now();
+        for (const nodeIdStr in Object.keys(response.data)) {
+            const nodeData=response.data[nodeId];
+            const nodeId=parseInt(nodeIdStr);
+            if(traffic_db[nodeId]){
+                //Insert into my memory db
+
+            }else{
+                //Create new mem-db entry
+                traffic_db[nodeId]=[];
+            }
+        }
+        for
         console.log(response);
     });
 }
